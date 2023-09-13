32e0067d4d205164f3a53858f82888d7acafaed3
zabooh <martin.ruppert@microchip.com>
2023-09-13 15:48:04 +0200

    h3 clone script added

---
+++ scripts/git_clone_t1s_h3.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+set H3_GITHUB="https://github.com/Microchip-MPLAB-Harmony"
+set H3_INSTALL="../../H3"
+
+git clone -b "v10.5.1"              https://github.com/ARM-software/CMSIS-FreeRTOS.git  %H3_INSTALL%/CMSIS-FreeRTOS  
+git clone -b "v3.17.0"              %H3_GITHUB%/csp.git  %H3_INSTALL%/csp   
+git clone -b "v3.13.0"              %H3_GITHUB%/core.git %H3_INSTALL%/core    
+git clone -b "v3.17.0"              %H3_GITHUB%/dev_packs.git %H3_INSTALL%/dev_packs    
+git clone -b "v5.4.0"               %H3_GITHUB%/wolfssl.git %H3_INSTALL%/wolfssl
+git clone -b "v3.9.2"               %H3_GITHUB%/net.git %H3_INSTALL%/net
+git clone -b "v3.8.0"               %H3_GITHUB%/crypto.git %H3_INSTALL%/crypto     
+git clone -b "v1.1.0"               %H3_GITHUB%/devices.git %H3_INSTALL%/Devices
+
+rem set net_10base_t1s to v1.3.0 by changing the yml file
+copy .\patches\harmony-manifest-success.yml  ..\apps\tcpip_iperf_lan865x\firmware\src\config\FreeRTOS\
+
+pause
+@ECHO ON
+
