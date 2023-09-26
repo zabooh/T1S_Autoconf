f2852d1752e23790d74582431a1c8b25548734c8
zabooh <martin.ruppert@microchip.com>
2023-09-26 16:39:12 +0200

    HEX file not of interest in build process

--- tools/makeit.bat
+++ tools/makeit.bat
@@ -1,16 +1,10 @@
 set MPLABX_TOOL_CHAIN=v6.15
 
-set HEX_FILE_01="..\apps\tcpip_iperf_lan867x\firmware\tcpip_iperf_lan867x_freertos.X\dist\FreeRTOS\production\tcpip_iperf_lan867x_freertos.X.production.hex"
-set HEX_FILE_02="..\apps\tcpip_iperf_lan867x\firmware\tcpip_iperf_lan867x_freertos.X\dist\FreeRTOS_node_1\production\tcpip_iperf_lan867x_freertos.X.production.hex"
-set HEX_FILE_03="..\apps\tcpip_iperf_lan867x\firmware\tcpip_iperf_lan867x_freertos.X\dist\FreeRTOS_node_2\production\tcpip_iperf_lan867x_freertos.X.production.hex"
-set HEX_FILE_04="..\apps\tcpip_iperf_lan867x\firmware\tcpip_iperf_lan867x_freertos.X\dist\FreeRTOS_node_3\production\tcpip_iperf_lan867x_freertos.X.production.hex"
-set MDB_PATH="c:\Program Files\Microchip\MPLABX\%MPLABX_TOOL_CHAIN%\mplab_platform\bin\mdb.bat"
-
 pushd .
 cd ..\apps\tcpip_iperf_lan867x\firmware\tcpip_iperf_lan867x_freertos.X
-make CONF=FreeRTOS
-make CONF=FreeRTOS_node_1
-make CONF=FreeRTOS_node_2
-make CONF=FreeRTOS_node_3
+make CONF=FreeRTOS -j 8
+make CONF=FreeRTOS_node_1 -j 8
+make CONF=FreeRTOS_node_2 -j 8
+make CONF=FreeRTOS_node_3 -j 8
 popd
 
